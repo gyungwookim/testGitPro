3abb39609096cc46d173b4784e6f89b3a68fe0e4
gyungwoo_kim <woo0075@nate.com>
2016-03-02 18:31:47 +0900

    ttt

--- testGit/app/src/main/java/com/yeoksoon/testgit/MainActivity.java
+++ testGit/app/src/main/java/com/yeoksoon/testgit/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.d("kkw", "test1");
+        Log.d("kkw", "test2");
+        Log.d("kkw", "test3");
 
 
     }
